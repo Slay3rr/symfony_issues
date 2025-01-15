6dbd59f19fcdf426f07075a7f55658230acb3bca
zenaba12 <alizenap433@gmail.com>
2025-01-15 12:39:28 +0100

    probléme73 remplacement de 'il' par 'ul'

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 docker run --name mysql_container -p 3309:3306 -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=symfony_issues mysql
 
-docker exec -it mysql_container bash
+docker exec -it mysql_container
--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,8 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
-
+use Symfony\Component\Routing\Attribute\Route ;
 final class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
